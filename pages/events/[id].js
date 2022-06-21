053a9392b1175fb792264553613acda5f33a5a97
Henrique Vieira <henrique@mvstudio.be>
2022-06-21 22:44:58 +0200

    grab cloudinary image from strapi

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -3,11 +3,10 @@ import ShowCase from "../../components/ShowCase";
 import { CMS_URL } from "../../lib/variables";
 
 export default function SingleEvent({ evt }) {
-
-
+    console.log(evt);
     return (
         <div className="single-event">
-            <ShowCase title={evt.attributes.title} subtitle={evt.attributes.subtitle} />
+            <ShowCase title={evt.attributes.title} subtitle={evt.attributes.subtitle} img={evt.attributes.cover.data.attributes.formats.large.url} />
             <div className="container">
                 <div className="wrapper"></div>
             </div>
@@ -16,7 +15,7 @@ export default function SingleEvent({ evt }) {
 }
 
 export async function getStaticProps({ params }) {
-    const { data } = await axios.get(CMS_URL + "/events/" + params.id);
+    const { data } = await axios.get(CMS_URL + "/events/" + params.id + "?populate=*");
     return {
         props: {
             evt: data.data,

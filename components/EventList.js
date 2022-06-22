d1b790ff0a897a63057334cd5e9d4eeb1547c6ec
Henrique Vieira <henrique@mvstudio.be>
2022-06-22 17:09:22 +0200

    add search filter to list of events

--- components/EventList.js
+++ components/EventList.js
@@ -1,12 +1,23 @@
+import { useEffect, useState } from "react";
 import Card from "./Card";
 
 export default function EventList({ events }) {
+    const [searchValue, setSearchValue] = useState("");
+    const [displayedEvt, setDisplayedEvt] = useState(events);
+
+    useEffect(() => {
+        setDisplayedEvt(events.filter((evt) => evt.attributes.title.toLowerCase().includes(searchValue.toLowerCase())));
+    }, [searchValue, events]);
+
     return (
-        <div className="event-list">
-            {events &&
-                events.map((evt) => {
-                    return <Card key={evt.id} evt={evt} />;
-                })}
-        </div>
+        <>
+            <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} placeholder="Search events"/>
+            <div className="event-list">
+                {events &&
+                    displayedEvt.map((evt) => {
+                        return <Card key={evt.id} evt={evt} />;
+                    })}
+            </div>
+        </>
     );
 }
